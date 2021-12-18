3957648fb0b72508d49e826cfc9b8e90b024be36
Rommel965 <5865@itescam.edu.mx>
2021-12-18 01:24:20 -0600

    titulos

--- resources/views/addalumnos/addalumnoscuatro.blade.php
+++ resources/views/addalumnos/addalumnoscuatro.blade.php
@@ -1,20 +1,35 @@
 @extends('templates.plantilla')
 
+
 @section('titulo', 'Inicio')
 
 @section('contenido')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <style>
+        body{
+            font-family: sans-serif
+        }
 
-       <!DOCTYPE html>
-       <html lang="en">
-       <head>
-           <meta charset="UTF-8">
-           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-           <meta http-equiv="X-UA-Compatible" content="ie=edge">
-           <title>Document</title>
-       </head>
-       <body>
-           <h1>pruebaaa</h1>
-       </body>
-       </html>  
+        .contenedor{
+            height: 20vh;
+            margin: 5%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="contenedor">
+    <h1>PROYECTOS DISPONIBLES</h1>
+</body>
+</html>
+
 
 @endsection
