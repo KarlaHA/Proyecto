735c5dd2ffa589380399f79b92de23035540a4ef
Rommel965 <5865@itescam.edu.mx>
2022-01-07 20:37:29 -0600

    modulo final

--- resources/views/asigcalificaciones/asigcalificacionesdos.blade.php
+++ resources/views/asigcalificaciones/asigcalificacionesdos.blade.php
@@ -6,7 +6,29 @@
 
 <section>
         <div class="main-container">
-           
+            <section>
+                <div class="table-container">
+                    <table class="table table-light table-bordered">
+                        <tbody>
+                            <tr>
+                                <th width="15%" class="text-center">Matricula</th>
+                                <th width="20%" class="text-center">Nombre</th>
+                                <th width="15%" class="text-center">Carrera</th>
+                                <th width="15%" class="text-center">Calif 1P</th>
+                                <th width="15%" class="text-center">Calif 2P</th>
+                                <th width="15%" class="text-center">Calif final</th>
+                            </tr>
+                            
+                            
+
+                                         
+                        </tbody>
+                    </table>
+                </div>
+
+                <a href="empresa"><button class="btn btn-primary" type="button">Terminar</button></a><br><br>
+
+            </section>  
         </div>
     </section>
 @endsection
--- resources/views/asigcalificaciones/asigcalificacionesuno.blade.php
+++ resources/views/asigcalificaciones/asigcalificacionesuno.blade.php
@@ -6,7 +6,23 @@
 
 <section>
         <div class="main-container">
-           
-        </div>
+
+        <form action="">
+            
+            <select name="periodo" id="periodo">
+            <option selected='selected' disabled>Carrera</option>
+            <option value="Ingenieria Bioquimica">Ingenieria Bioquimica</option>
+                        <option value="Ingenieria en Industrias Alimentarias">Ingenieria en Industrias Alimentarias</option>
+                        <option value="Ingenieria en Materiales">Ingenieria en Materiales</option>
+                        <option value="Ingenieria en Sistemas Computacionales">Ingenieria en Sistemas Comptacionales</option>
+                        <option value="Ingenieria Indstrial">Ingenieria Industrial</option>
+                        <option value="Ingenieria Mecatronica">Ingenieria Mecatronica</option>
+                        <option value="Licenciatura en Administracion">Licenciatura en Administracion</option>
+                        <option value="Ingenieria Informatica">Ingenieria Informatica</option>
+                        <option value="Ingenieria en Innovacion Agricola">Ingenieria en Innovacion Agricola</option>
+            </select>
+            
+
+        </form>
     </section>
 @endsection
